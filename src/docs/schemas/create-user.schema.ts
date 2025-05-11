2a20b4b58ac92cb32818e8a7d1e9b93c044ca42d
Andrew Kononenko <wastardy.k@gmail.com>
2025-05-11 11:59:40 +0300

    chore(schema): move CreateUser schema to separate file for better organization

---
+++ src/docs/schemas/create-user.schema.ts
@@ -0,0 +1,35 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     CreateUser:
+ *       type: object
+ *       properties:
+ *         chatId:
+ *           type: string
+ *           format: int64
+ *           description: Unique identifier for the telegram chat
+ *           example: "123456789012345678"
+ *         username:
+ *           type: string
+ *           description: User's telegram username
+ *           nullable: true
+ *           example: "wastardy"
+ *         nickname:
+ *           type: string
+ *           description: User's telegram nickname
+ *           nullable: true
+ *           example: "Andrew"
+ *         groupId:
+ *           type: integer
+ *           description: Unique identifier for the group
+ *           example: 1
+ *         step:
+ *           type: string
+ *           enum: ["ChooseNickname", "ChooseGroup", "MainMenu"]
+ *           description: Current step of the user in the bot
+ *       required:
+ *         - chatId
+ *         - groupId
+ *         - step
+ */
