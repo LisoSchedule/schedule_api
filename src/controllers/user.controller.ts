de2ef379c30922d21633cb295780253cb8f8fc1e
Andrew Kononenko <wastardy.k@gmail.com>
2025-05-11 16:13:42 +0300

    feat(controller): add UserController with createUser method for user creation

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+
+import { UserService } from "../services/user.service";
+import { User } from "@prisma/client";
+import { SuccessResponseDto } from "../dtos/success-response.dto";
+import { CreateUserDto } from "../dtos/create-user.dto";
+import { CurrentUserDto } from "../dtos/current-user.dto";
+import successConstant from "../constants/success.constant";
+import errorConstant from "../constants/error.constant";
+
+export class UserController {
+  private readonly userService: UserService = new UserService();
+
+  async createUser(req: Request, res: Response) {
+    const data: CreateUserDto = {
+      body: req.body,
+    };
+
+    const newUser: User = await this.userService.createUser(data);
+
+    res.status(201).json(
+      new SuccessResponseDto({
+        user: new CurrentUserDto(newUser),
+        message: successConstant.CREATED,
+      }),
+    );
+  }
+}
