44573384005bc9e463d3e0c12478b553c7824971
Andrew Kononenko <wastardy.k@gmail.com>
2025-05-11 16:13:32 +0300

    fix(validator): update CreateUserSchema to use coerced bigint for chatId and enum for step

--- src/validators/create-user.validator.ts
+++ src/validators/create-user.validator.ts
@@ -3,10 +3,10 @@ import { UserStep } from "@prisma/client";
 
 export const CreateUserSchema = zod.object({
   body: zod.object({
-    chatId: zod.bigint(),
+    chatId: zod.coerce.bigint(),
     username: zod.string().optional().nullable(),
     nickname: zod.string().optional().nullable(),
     groupId: zod.coerce.number().int().positive(),
-    step: zod.nativeEnum(UserStep),
+    step: zod.enum([UserStep.ChooseNickname, UserStep.ChooseGroup, UserStep.MainMenu]),
   }),
 });
