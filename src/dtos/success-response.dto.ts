5384e80a539fff5e85050354c4635316f1ce26d8
Andrew Kononenko <wastardy.k@gmail.com>
2025-05-18 00:30:48 +0300

    feat(dto): enhance SuccessResponseDto to include an optional message parameter

--- src/dtos/success-response.dto.ts
+++ src/dtos/success-response.dto.ts
@@ -1,6 +1,6 @@
 export class SuccessResponseDto<T> {
-  constructor(data: T) {
-    Object.assign(this, { success: true, data });
+  constructor(data: T, message?: string) {
+    Object.assign(this, { success: true, message, data });
   }
 
   success!: boolean;
