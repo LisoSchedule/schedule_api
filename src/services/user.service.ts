f3c2d9b86694e2e6532b95fb1406631bb994a8b3
Andrew Kononenko <wastardy.k@gmail.com>
2025-05-21 01:13:31 +0300

    feat(service): implement updateUser method for user updates

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,13 +1,13 @@
-import { Request } from "express";
 import { BadRequest, NotFound } from "http-errors";
 
-import { Prisma } from "@prisma/client";
+import { Prisma, User, UserSettings } from "@prisma/client";
 import { UserRepository } from "../repositories/user.repository";
 import { CreateUserDto } from "../dtos/create-user.dto";
+import { UpdateUserDto } from "../dtos/update-user.dto";
 import errorConstant from "../constants/error.constant";
 
 export class UserService {
-  private readonly userRepository: UserRepository = new UserRepository();
+  constructor(private readonly userRepository: UserRepository = new UserRepository()) {}
 
   async createUser(data: CreateUserDto) {
     const { chatId, username, nickname, groupId } = data.body || data;
@@ -36,5 +36,25 @@ export class UserService {
     }
 
     return newUser;
+  }
+
+  async updateUser(chatId: bigint, data: UpdateUserDto) {
+    const { nickname } = data;
+
+    const existingUser = await this.userRepository.findUserByChatId(chatId);
+
+    if (!existingUser) {
+      throw new NotFound(errorConstant.USER_NOT_FOUND);
+    }
+
+    const updatedUser: User = await this.userRepository.updateUser(existingUser.id, {
+      nickname: nickname,
+    });
+
+    if (!updatedUser) {
+      throw new BadRequest(errorConstant.BAD_REQUEST);
+    }
+
+    return updatedUser;
   }
 }
