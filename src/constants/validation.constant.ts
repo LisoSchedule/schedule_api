6a0986cfb9c59ebb40ada13b0991e033bd0c1c79
Andrew Kononenko <wastardy.k@gmail.com>
2025-05-21 01:16:09 +0300

    feat(constants): add user update success messages and validation constants

--- src/constants/validation.constant.ts
+++ src/constants/validation.constant.ts
@@ -1,1 +1,9 @@
-export default {} as const;
+export default {
+  CANNOT_BE_EMPTY: "Field cannot be empty",
+
+  FIELD_TOO_SHORT: "Field must be at least 2 characters long",
+  FIELD_TOO_LONG: "Field must be at most 30 characters long",
+
+  INVALID_BOOLEAN: "Field must be a boolean",
+  INVALID_ENUM_VALUE: "Invalid value for enum field",
+} as const;
