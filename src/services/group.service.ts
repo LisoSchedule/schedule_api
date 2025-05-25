0c99dcaabe9708e8278c09b6248b51c2282014a5
Andrew Kononenko <wastardy.k@gmail.com>
2025-05-26 01:42:02 +0300

    feat(service): implement getGroupById method in GroupService

--- src/services/group.service.ts
+++ src/services/group.service.ts
@@ -1,10 +1,15 @@
 import { BadRequest, NotFound } from "http-errors";
 
 import { GroupRepository } from "../repositories/group.repository";
+import { GroupWithRelationsMapper } from "../mappers/group-with-relations.mapper";
+import { GroupWithRelationsDto } from "../dtos/group-with-relations.dto";
 import errorConstant from "../constants/error.constant";
 
 export class GroupService {
-  constructor(private readonly groupRepository: GroupRepository = new GroupRepository()) {}
+  constructor(
+    private readonly groupRepository: GroupRepository = new GroupRepository(),
+    private readonly groupWithRelationsMapper: GroupWithRelationsMapper = new GroupWithRelationsMapper(),
+  ) {}
 
   async getAllGroups() {
     const groups = await this.groupRepository.getAllGroups();
@@ -16,5 +21,19 @@ export class GroupService {
     return groups;
   }
 
-  async getGroupById() {}
+  async getGroupById(groupId: number) {
+    if (!groupId) {
+      throw new BadRequest(errorConstant.BAD_REQUEST);
+    }
+
+    const group = await this.groupRepository.getGroupById(groupId);
+
+    if (!group) {
+      throw new NotFound(errorConstant.NOT_FOUND);
+    }
+
+    const groupWithRelationsDto: GroupWithRelationsDto = this.groupWithRelationsMapper.toDto(group);
+
+    return groupWithRelationsDto;
+  }
 }
