0f2a99c08109111c7822345ba7944b1b6fa9ad4d
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-03 23:36:35 +0300

    refactor(user.controller): streamline response handling by using structured success constants

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -10,6 +10,9 @@ import { UpdateUserSettingsDto } from "../dtos/update-user-settings.dto";
 import { UserWithSettingsDto } from "../dtos/user-with-settings.dto";
 import successConstant from "../constants/success.constant";
 
+const { USER_FETCHED, USER_CREATED, USER_UPDATED, USER_SETTINGS_UPDATED, USER_DELETED } =
+  successConstant;
+
 export class UserController {
   constructor(private readonly userService: UserService = new UserService()) {}
 
@@ -20,12 +23,9 @@ export class UserController {
 
     const userWithSettings = await this.userService.getUserWithSettingsById(userId);
 
-    res.status(201).json(
-      new SuccessResponseDto({
-        message: successConstant.USER_FETCHED,
-        user: userWithSettings,
-      }),
-    );
+    res
+      .status(USER_FETCHED.statusCode)
+      .json(new SuccessResponseDto(userWithSettings, USER_FETCHED.message));
   }
 
   async createUser(req: Request, res: Response) {
@@ -35,12 +35,9 @@ export class UserController {
 
     const newUser: User = await this.userService.createUser(data);
 
-    res.status(201).json(
-      new SuccessResponseDto({
-        message: successConstant.CREATED,
-        user: new CurrentUserDto(newUser),
-      }),
-    );
+    res
+      .status(USER_CREATED.statusCode)
+      .json(new SuccessResponseDto(new CurrentUserDto(newUser), USER_CREATED.message));
   }
 
   async updateUser(req: Request, res: Response) {
@@ -52,12 +49,9 @@ export class UserController {
 
     const updatedUser: User = await this.userService.updateUser(userId, updateUserDto);
 
-    res.status(200).json(
-      new SuccessResponseDto({
-        message: successConstant.USER_UPDATED,
-        user: new CurrentUserDto(updatedUser),
-      }),
-    );
+    res
+      .status(USER_UPDATED.statusCode)
+      .json(new SuccessResponseDto(new CurrentUserDto(updatedUser), USER_UPDATED.message));
   }
 
   async upsertUserSettings(req: Request, res: Response) {
@@ -73,12 +67,9 @@ export class UserController {
       updateUserSettingsDto,
     );
 
-    res.status(200).json(
-      new SuccessResponseDto({
-        message: successConstant.USER_SETTINGS_UPDATED,
-        user: userWithSettings,
-      }),
-    );
+    res
+      .status(USER_SETTINGS_UPDATED.statusCode)
+      .json(new SuccessResponseDto(userWithSettings, USER_SETTINGS_UPDATED.message));
   }
 
   async deleteUser(req: Request, res: Response) {
@@ -86,10 +77,6 @@ export class UserController {
 
     await this.userService.deleteUser(userId);
 
-    res.status(200).json(
-      new SuccessResponseDto({
-        message: successConstant.USER_DELETED,
-      }),
-    );
+    res.status(USER_DELETED.statusCode).json(new SuccessResponseDto(null, USER_DELETED.message));
   }
 }
