bf861b9ed831b17c650d7ad8fcf0dd5a53603b18
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-07 21:48:09 +0300

    feat(routes): add route to get subject by id

--- src/routes/subjects.route.ts
+++ src/routes/subjects.route.ts
@@ -49,3 +49,56 @@ SubjectsRouter.get(
   limiter(timeConstant.ONE_SECOND, 3, true),
   catchHandler(subjectController.getAllSubjects.bind(subjectController)),
 );
+
+/**
+ * @swagger
+ * /api/subjects/{subjectId}:
+ *   get:
+ *     summary: Get a specific subject by id
+ *     tags: [Subjects]
+ *     parameters:
+ *       - in: path
+ *         name: subjectId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *           example: 1
+ *         description: id of the subject to retrieve
+ *       - in: query
+ *         name: includeRelations
+ *         required: false
+ *         schema:
+ *           type: boolean
+ *           default: false
+ *         description: Whether to include related data
+ *     responses:
+ *       200:
+ *         description: Subject retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: "TEACHER_FETCHED"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     data:
+ *                       $ref: '#/components/schemas/Subject'
+ *       404:
+ *         description: Subject not found
+ *       429:
+ *         description: Too many requests
+ *       500:
+ *         description: Internal server error
+ */
+SubjectsRouter.get(
+  "/:subjectId",
+  limiter(timeConstant.ONE_SECOND, 3, true),
+  catchHandler(subjectController.getSubjectById.bind(subjectController)),
+);
