514c2f87af73dff2f515a5bbc7e735db5ffc2163
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-07 12:48:57 +0300

    refactor(error): update group service error handling, add group not found error constant

--- src/services/group.service.ts
+++ src/services/group.service.ts
@@ -1,9 +1,11 @@
-import { BadRequest, NotFound } from "http-errors";
+import createHttpError from "http-errors";
 
 import { GroupRepository } from "../repositories/group.repository";
 import { GroupWithRelationsMapper } from "../mappers/group-with-relations.mapper";
 import { GroupWithRelationsDto } from "../dtos/group-with-relations.dto";
 import errorConstant from "../constants/error.constant";
+
+const { GROUP_NOT_FOUND, NOT_FOUND } = errorConstant;
 
 export class GroupService {
   constructor(
@@ -15,7 +17,7 @@ export class GroupService {
     const groups = await this.groupRepository.getAllGroups();
 
     if (!groups || groups.length === 0) {
-      throw new NotFound(errorConstant.NOT_FOUND);
+      throw createHttpError(NOT_FOUND.statusCode, NOT_FOUND.message);
     }
 
     return groups;
@@ -23,13 +25,13 @@ export class GroupService {
 
   async getGroupById(groupId: number) {
     if (!groupId) {
-      throw new BadRequest(errorConstant.BAD_REQUEST);
+      throw createHttpError(errorConstant.BAD_REQUEST);
     }
 
     const group = await this.groupRepository.getGroupById(groupId);
 
     if (!group) {
-      throw new NotFound(errorConstant.NOT_FOUND);
+      throw createHttpError(GROUP_NOT_FOUND.statusCode, GROUP_NOT_FOUND.message);
     }
 
     const groupWithRelationsDto: GroupWithRelationsDto = this.groupWithRelationsMapper.toDto(group);
