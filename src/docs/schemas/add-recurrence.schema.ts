d8ef894b163400c8d8352a09189260a067ad5416
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-08 21:38:23 +0300

    fix(schema): update add-recurrence schema with date-time format and improved descriptions

--- src/docs/schemas/add-recurrence.schema.ts
+++ src/docs/schemas/add-recurrence.schema.ts
@@ -9,30 +9,28 @@
  *           type: integer
  *           format: int32
  *           description: id of the lesson to create recurrence
- *           example: "1"
- *         startDate:
- *           type: integer
- *           format: int32
- *           minimum: 1
- *           description: Sub-group number
  *           example: 1
+ *         startDate:
+ *           type: string
+ *           format: date-time
+ *           description: First date lesson proceeds
+ *           example: "2024-09-01T08:30:00Z"
  *         endDate:
- *           type: integer
- *           format: int32
- *           minimum: 1
- *           description: Sub-group number
+ *           type: string
+ *           format: date-time
+ *           description: Last date lesson proceeds
+ *           example: "2025-06-15T10:55:00Z"
  *         repeatType:
  *           type: string
- *           description: Repeat type
+ *           description: Type of recurrence pattern (Daily, Weekly, Monthly)
  *           enum: [Daily, Weekly, Monthly]
- *           example: "Daily"
+ *           example: "Weekly"
  *         repeatValue:
  *           type: integer
  *           format: int32
  *           minimum: 1
- *           description: Repeat value
- *           example: 1
- *           example: 1
+ *           description: For Daily - every `X` days, for Weekly - every `X` weeks, for Monthly - every `X` months
+ *           example: 2
  *       required:
  *         - lessonId
  *         - startDate
--- src/validators/add-recurrence.validator.ts
+++ src/validators/add-recurrence.validator.ts
@@ -5,8 +5,8 @@ import validationConstant from "../constants/validation.constant";
 
 export const AddRecurrenceSchema = zod.object({
   body: zod.object({
-    lessonId: zod.number().int(),
-    startDate: zod.string().datetime(),
+    lessonId: zod.number().int(validationConstant.CANNOT_BE_EMPTY),
+    startDate: zod.string().datetime(validationConstant.INVALID_DATE_TIME_FORMAT),
     endDate: zod.string().datetime(),
     repeatType: zod.nativeEnum(RepeatType, { message: validationConstant.INVALID_ENUM_VALUE }),
     repeatValue: zod.coerce.number().int().positive(validationConstant.MUST_BE_POSITIVE_INTEGER),
