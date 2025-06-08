dd8d70ddc59229c5e0ebecd2deeaef7668e31509
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-08 08:18:41 +0300

    feat(dto): add dto, validator and swagger schema for creating audiences

---
+++ src/docs/schemas/add-audience.schema.ts
@@ -0,0 +1,14 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     AddAudience:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: Audience's name
+ *           example: "Аудиторія 47"
+ *       required:
+ *         - name
+ */
