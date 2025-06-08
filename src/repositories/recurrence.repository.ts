c416704972ef2c0193aac0f7475cfacc4667fc06
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-08 18:07:23 +0300

    feat(repository): add recurrence repository with methods for retrieving and creating recurrences

---
+++ src/repositories/recurrence.repository.ts
@@ -0,0 +1,26 @@
+import { LessonRecurrence, Prisma } from "@prisma/client";
+import prisma from "../db/data-sourse";
+
+export class RecurrenceRepository {
+  private readonly recurrenceRepository = prisma.lessonRecurrence;
+
+  async getAllRecurrences() {
+    return await this.recurrenceRepository.findMany();
+  }
+
+  async getRecurrenceById(recurrenceId: number) {
+    return await this.recurrenceRepository.findUnique({
+      where: {
+        id: recurrenceId,
+      },
+    });
+  }
+
+  async createRecurrence(
+    recurrenceToCreate: Prisma.LessonRecurrenceCreateInput,
+  ): Promise<LessonRecurrence> {
+    return await this.recurrenceRepository.create({
+      data: recurrenceToCreate,
+    });
+  }
+}
